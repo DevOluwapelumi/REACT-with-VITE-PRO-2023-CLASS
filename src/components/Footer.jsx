6cf3a089e6376e9daed55515639910eba509a1bb
OGUNDIRAN VICTOR ISREAL <victoroluwadarasimi16@gmail.com>
2023-10-09 09:38:46 +0100

    second pro commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
-
+import styles from "../styles/styles";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div style = {styles.footer}>
+      <h1>Printivo</h1>
+      <ul>
+    <li>About</li>
+    <li>About</li>
+    <li>About</li>
+    <li>About</li>
+    <li>About</li>
+    <li>About</li>
+    </ul>
+    </div>
   )
 }
 
